33de84fcb9f74b43a2a0937ab417db23285940a2
LabSR-UT <142353737+LabSR-UT@users.noreply.github.com>
2025-03-08 16:12:10 -0500

    Add files via upload

---
+++ ImaSR_Q1.py
@@ -0,0 +1,25 @@
+# Imagenes SR
+# Quiz 1
+
+import streamlit as st
+import numpy as np
+from skimage.io import imread
+
+st.title('Quiz 1')
+st.subheader('Pregunta 1:')
+
+valor1 = st.radio("Cual fue la razon que el satelite Landsat 5 duro mas tiempo funcionando?",
+    ["Calidad de los componentes", "Cantidad de combustible", "Orbita baja"],
+	index=None,
+)
+
+st.write("Usted selecciono: ", valor1)
+
+st.subheader('Pregunta 2:')
+
+valor2 = st.radio("Cual fue el principal aporte del satelite Landsat5 al programa Landsat?",
+    ["Mayor cantidad de sensores", "Mayor capacidad de almacenamiento", "Permitio dar continuidad al programa"],
+	index=None,
+)
+
+st.write("Usted selecciono: ", valor2)
